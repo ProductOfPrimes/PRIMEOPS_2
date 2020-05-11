7f0cb3f23d960cb093b9fea3d276d269aaf81cda
Jacky Yang <jackyyang267@gmail.com>
2020-05-11 11:51:28 -0400

    Enhanced Main Menu sequence

---
+++ Source/PRIMEOPS_2/PRIMEOPS_2.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PRIMEOPS_2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PRIMEOPS_2, "PRIMEOPS_2" );
