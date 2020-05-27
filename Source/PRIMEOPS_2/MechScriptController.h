817e21ac7f6f07476c15ca62aa19614ccbf9f794
Matthew <mattdemoe@gmail.com>
2020-05-27 14:30:32 -0400

    Added walking functionality to c++ script

--- Source/PRIMEOPS_2/MechScriptController.h
+++ Source/PRIMEOPS_2/MechScriptController.h
@@ -3,7 +3,10 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Engine.h"
 #include "GameFramework/PlayerController.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/Pawn.h"
 #include "MechScriptController.generated.h"
 
 /**
@@ -18,9 +21,15 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaSeconds) override;
 
+	UFUNCTION(BlueprintCallable)
+	float GetSpeed() { return speed; }
+
 protected:
 
-	UPROPERTY(EditAnywhere, Category = Aiming)
+	FVector m_leftStickInput = FVector();
+	FVector m_rightStickInput = FVector();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Aiming)
 	FVector aimDirection = FVector(0.0f, 0.0f, 0.0f);
 
 	UPROPERTY(EditAnywhere, Category = Aiming)
@@ -32,32 +41,34 @@ protected:
 	UPROPERTY(EditAnywhere, Category = Aiming)
 	float turnSpeed = 145.0f;
 
-	UPROPERTY(EditAnywhere, Category = Movement)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement)
 	float speed = 0.0f;
 
 	UPROPERTY(EditAnywhere, Category = Movement)
 	float speedMax = 7500.0f;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement)
+	float velocityDotProduct = 0.0f;
+
 	float m_pivotSpeed = 2.0f;
 	float m_accelerationMax = 140.0f;
-	float m_linearDrag = 0.1f;
-	float m_exponentialDrag = 0.975f;
-	float m_velocityDotProduct = 0.0f;
+	float m_linearDrag = 50.0f;
+	float m_exponentialDrag = 0.9f;
 	float m_turnAngle = 0.0f;
 
-	UPROPERTY(EditAnywhere, Category = Movement)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement)
 	FVector velocity = FVector(0.0f, 0.0f, 0.0f);
 
 	UPROPERTY(EditAnywhere, Category = Movement)
 	FVector desiredVelocity = FVector(0.0f, 0.0f, 0.0f);
 
-	UPROPERTY(EditAnywhere, Category = Movement)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement)
 	FVector walkDirection = FVector(0.0f, 0.0f, 0.0f);
 
 	FVector m_lastDirInput = FVector(0.0f, 0.0f, 0.0f);
 	FVector m_desiredDeltaVelocity = FVector(0.0f, 0.0f, 0.0f);
 
-	UPROPERTY(EditAnywhere, Category = Movement)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement)
 	FRotator walkRotation = FRotator(0.0f, 0.0f, 0.0f);
 
 	bool m_isTurning = false;
@@ -70,5 +81,10 @@ protected:
 
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	void PollControllers();
+	void InputToVelocity(float DeltaSeconds);
+	void GetWalkDirection(float DeltaSeconds);
+	void DecaySpeed(float DeltaSeconds);
 
 };
