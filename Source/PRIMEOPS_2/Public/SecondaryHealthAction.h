88551ccf2b8ff8c849df11fd68c856a45f4c12ee
Matthew <mattdemoe@gmail.com>
2020-06-15 10:02:34 -0400

    UI Stuff

--- Source/PRIMEOPS_2/Public/SecondaryHealthAction.h
+++ Source/PRIMEOPS_2/Public/SecondaryHealthAction.h
@@ -1,6 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
+#include "Blueprint/UserWidget.h"
+#include "Runtime/UMG/Public/UMG.h"
+#include "Slate.h"
 #include "LatentActions.h"
 #include "CoreMinimal.h"
 
@@ -31,7 +34,19 @@ public:
 	{
 		timer -= Response.ElapsedTime();
 
+	
+
 		Response.FinishAndTriggerIf(timer <= 0.0f, ExecutionFunction, OutputLink, CallbackTarget);
 	}
 
+#if WITH_EDITOR
+	// Returns a human readable description of the latent operation's current state
+	virtual FString GetDescription() const override
+	{
+		static const FNumberFormattingOptions DelayTimeFormatOptions = FNumberFormattingOptions()
+			.SetMinimumFractionalDigits(3)
+			.SetMaximumFractionalDigits(3);
+		return FText::Format(NSLOCTEXT("SecondaryHealthAction", "SecondaryHealthTimeFmt", "Delay ({0} seconds left)"), FText::AsNumber(timer, &DelayTimeFormatOptions)).ToString();
+	}
+#endif
 };
