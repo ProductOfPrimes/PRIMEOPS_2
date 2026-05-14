6304af7fdcf409ada23831b3f74e6fda0228c4b8
JossKM <jossmooyoung@gmail.com>
2026-05-14 00:47:27 -0400

    Got it running in 5.7, nice

--- Source/PRIMEOPS_2.Target.cs
+++ Source/PRIMEOPS_2.Target.cs
@@ -8,7 +8,7 @@ public class PRIMEOPS_2Target : TargetRules
 	public PRIMEOPS_2Target(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
 
 		ExtraModuleNames.AddRange( new string[] { "PRIMEOPS_2" } );
 	}
